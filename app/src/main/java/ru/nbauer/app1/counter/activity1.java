487c2a661bb2192ae739e105f63357a924940f13
romasan <kotapesic@gmail.com>
2017-02-15 11:38:44 +0300

    Add files

---
+++ app/src/main/java/ru/nbauer/app1/counter/activity1.java
@@ -0,0 +1,27 @@
+package ru.nbauer.app1.counter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//import android.widget.TextView;
+//import android.view.View;
+
+// https://habrahabr.ru/post/114580/
+
+public class activity1 extends AppCompatActivity {
+
+//    private TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity1);
+
+//         text = (TextView)findViewById(R.id.textView);
+    }
+
+// android:onClick="but1_click"
+//    public void btn1_click(View v){
+//         text.setText("M3SS4G3!");
+//    }
+}
