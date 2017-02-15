220695d06cd1b4ec329be9e51dbbde0a06d0cffb
roman <kotapesic@gmail.com>
2017-02-15 17:21:19 +0300

    Dev

--- app/src/main/java/ru/nbauer/app1/counter/activity1.java
+++ app/src/main/java/ru/nbauer/app1/counter/activity1.java
@@ -1,27 +1,99 @@
 package ru.nbauer.app1.counter;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-//import android.widget.TextView;
-//import android.view.View;
+import android.widget.TextView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.content.res.Resources;
+
+//import com.google.android.gms.appindexing.Action;
+//import com.google.android.gms.appindexing.AppIndex;
+//import com.google.android.gms.appindexing.Thing;
+//import com.google.android.gms.common.api.GoogleApiClient;
 
 // https://habrahabr.ru/post/114580/
 
 public class activity1 extends AppCompatActivity {
 
-//    private TextView text;
+    private TextView text;
+    private String code;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+//    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity1);
 
-//         text = (TextView)findViewById(R.id.textView);
+        text = (TextView) findViewById(R.id.codeText);
+        code = "";
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+//        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-// android:onClick="but1_click"
-//    public void btn1_click(View v){
-//         text.setText("M3SS4G3!");
+    public OnClickListener myListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            int id = v.getId();                       // get integer id of view
+            String idString = "no id";
+            if (id != View.NO_ID) {                    // make sure id is valid
+                Resources res = v.getResources();     // get resources
+                if (res != null)
+                    idString = res.getResourceEntryName(id); // get id string entry
+            }
+
+            // String id = Button.getTag().toString();
+            String result = "click anything: " + idString;
+            text.setText(result);
+        }
+    };
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+//    public Action getIndexApiAction() {
+//        Thing object = new Thing.Builder()
+//                .setName("activity1 Page") // TODO: Define a title for the content shown.
+//                // TODO: Make sure this auto-generated URL is correct.
+//                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+//                .build();
+//        return new Action.Builder(Action.TYPE_VIEW)
+//                .setObject(object)
+//                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+//                .build();
+//    }
+
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//
+//        // ATTENTION: This was auto-generated to implement the App Indexing API.
+//        // See https://g.co/AppIndexing/AndroidStudio for more information.
+//        client.connect();
+//        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+//    }
+
+//    @Override
+//    public void onStop() {
+//        super.onStop();
+//
+//        // ATTENTION: This was auto-generated to implement the App Indexing API.
+//        // See https://g.co/AppIndexing/AndroidStudio for more information.
+//        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+//        client.disconnect();
 //    }
 }
+
+//    android:onClick="but1_click"
+//    public void btn1_click(View v){
+//        text.setText("M3SS4G3!" + v);
+//    }
