27a299d6a65d47f5a80be631eed70b814c9a1ea3
romasan <kotapesic@gmail.com>
2017-02-17 11:11:50 +0300

    Dev

--- app/src/main/java/ru/nbauer/app1/counter/activity1.java
+++ app/src/main/java/ru/nbauer/app1/counter/activity1.java
@@ -1,21 +1,25 @@
 package ru.nbauer.app1.counter;
 
-import android.net.Uri;
+//import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.widget.TextView;
+import android.widget.Button;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.content.res.Resources;
+//import android.content.res.Resources;
+import java.util.Objects;
 
 //import com.google.android.gms.appindexing.Action;
 //import com.google.android.gms.appindexing.AppIndex;
 //import com.google.android.gms.appindexing.Thing;
 //import com.google.android.gms.common.api.GoogleApiClient;
 
+// README
 // https://habrahabr.ru/post/114580/
-//https://goo.gl/JjYzCg
+// https://goo.gl/JjYzCg
+// https://github.com/romasan/old-java-projects/blob/master/twitsearcher/src2/citadel.java
 
 public class activity1 extends AppCompatActivity {
 
@@ -34,28 +38,56 @@ public class activity1 extends AppCompatActivity {
 
         text = (TextView) findViewById(R.id.codeText);
         code = "";
+
+        int[] buttons = {
+                R.id.button0,
+                R.id.button1,
+                R.id.button2,
+                R.id.button3,
+                R.id.button4,
+                R.id.button5,
+                R.id.button6,
+                R.id.button7,
+                R.id.button8,
+                R.id.button9,
+                R.id.button_clear,
+                R.id.button_done
+        };
+        for (int i = 0; i < buttons.length; i += 1) {
+            Button buttonNum = (Button) findViewById(buttons[i]);
+            buttonNum.setOnClickListener(doSomething);
+        }
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
 //        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    public OnClickListener myListener = new OnClickListener() {
+    private OnClickListener doSomething = new OnClickListener() {
         @Override
         public void onClick(View v) {
-
-            int id = v.getId();                       // get integer id of view
-            String idString = "no id";
-            if (id != View.NO_ID) {                    // make sure id is valid
-                Resources res = v.getResources();     // get resources
-                if (res != null)
-                    idString = res.getResourceEntryName(id); // get id string entry
+            String tag = v.getTag().toString();
+            if(Objects.equals(tag, "clear")) {
+                int length = code.length();
+                if(length > 0) {
+                    code = code.substring(0, length - 1);
+                }
+            } else if(Objects.equals(tag, "done")) {
+                code = "D O N E";
+            } else {
+                code += tag;
             }
-
-            // String id = Button.getTag().toString();
-            String result = "click anything: " + idString;
-            text.setText(result);
+            text.setText(code);
         }
     };
+
+//        int id = v.getId();                       // get integer id of view
+//        String idString = "no id";
+//        if (id != View.NO_ID) {                    // make sure id is valid
+//            Resources res = v.getResources();     // get resources
+//            if (res != null)
+//                idString = res.getResourceEntryName(id); // get id string entry
+//        }
+
 
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
